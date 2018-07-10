e7434a8ef638058062b736cab54b792ef041bcaf
Marcel Smit <marcel303@gmail.com>
2018-07-10 23:36:31 +0200

    Began porting CIsosurface test to Framework.

--- test/CIsosurface/main.cpp
+++ test/CIsosurface/main.cpp
@@ -2,12 +2,10 @@
 // CIsosurface text.
 //////////////////////////////////////////////////////////////////////
 
-#include <alleggl.h>
-#include <gl/glu.h>
 #include <math.h>
 #include "marstd.cpp"
-#include "../util/SOpenGL.h"
-#include "../util/SOpenGLTextureManager.h"
+#include "../Util/SOpenGL.h"
+#include "framework.h"
 
 //////////////////////////////////////////////////////////////////////
 // Could also use isosurfaces to do csg add, substract, xor, etc like in some raytracers.
@@ -22,22 +20,24 @@ int main(int argc, char* argv[])
 //--------------------------------------------------------------------
 // Initialize system.
 
-	allegro_init();
-	
+#if 0
 	allegro_message("MarSTD\n------\nCIsosurface & CIsosurfaceMetaball classes example.\nPress L to render as wireframe.\nPress I to invert metaball energy values.\nPress mouse button 1 & 2 to change treshold.\n------\nLogic execution rate is currently equal to the number of frames per second it renders.\nIf the example appears to execute too fast, enable vsync in your display properties for OpenGL applications.\n------\nMarcel Smit, 2003.");
 
 	if (install_timer() < 0 || install_keyboard() < 0 || install_mouse() < 0)
 	{
 		allegro_message("Error: unable to install system drivers.");
 		return -1;
-	}		
+	}
+#endif
 	
 //--------------------------------------------------------------------
 // Open an OpenGL window.
 
-	if (SOpenGL::I().setGraphicsMode(800, 600, desktop_color_depth(), false) < 0)
+	framework.enableDepthBuffer = true;
+	
+	if (!framework.init(0, nullptr, 800, 600))
 	{
-		allegro_message("Error: unable to set OpenGL graphics mode.");
+		//allegro_message("Error: unable to set OpenGL graphics mode.");
 		return -1;
 	}
 	
@@ -55,28 +55,28 @@ int main(int argc, char* argv[])
 	
 	// Textures.
 	
-	GLuint envmap = SOpenGLTextureManager::I().addTextureFromFile("data/envmap.bmp");
-	GLuint texmap = SOpenGLTextureManager::I().addTextureFromFile("data/texmap.bmp");
+	GLuint envmap = getTexture("data/envmap.bmp");
+	GLuint texmap = getTexture("data/texmap.bmp");
  	
 //--------------------------------------------------------------------
 // Main loop.
  	
-	while (!key[KEY_ESC])
+	while (!keyboard.wentDown(SDLK_ESCAPE))
 	{
 	
+		framework.process();
+		
 		static float t = 0.0;
 		static float ry = 0.0;
 	
 //--------------------------------------------------------------------
 // Input.
 	
-		int mx, my;
-		get_mouse_mickeys(&mx, &my);
-		ry += mx;
+		ry += mouse.dx;
 		
-		if (mouse_b & 1)
+		if (mouse.isDown(BUTTON_LEFT))
 			field.treshold += 0.01;
-		if (mouse_b & 2)
+		if (mouse.isDown(BUTTON_RIGHT))
 			field.treshold -= 0.01;			
    	
 //--------------------------------------------------------------------
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
 
 		// Invert?
 		
-		if (key[KEY_I])
+		if (keyboard.isDown(SDLK_i))
 		{
 			for (int i = 0; i < field.sx; ++i)
 				for (int j = 0; j < field.sy; ++j)
@@ -150,20 +150,12 @@ int main(int argc, char* argv[])
 		
 //--------------------------------------------------------------------
 // Render.
+
+		framework.beginDraw(0, 0, 0, 0);
 		
 		// Setup matrices.
-
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
 		
-		gluPerspective(90.0, SCREEN_W / (float)SCREEN_H, 0.001, 100.0);
-		
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		
-		// Prepare buffers.
-		
-		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+		projectPerspective3d(90.f, .001f, 100.f);
 		
 		glDepthFunc(GL_LESS);
 		glEnable(GL_DEPTH_TEST);		
@@ -185,7 +177,8 @@ int main(int argc, char* argv[])
 			{ 0.5, 0.5, 1.5, 1.0 },
 			{ 2.0, 2.0, 2.0, 1.0 }
 		};
-			
+	
+	#if 0 // todo
 		for (int i  =0; i < 4; ++i)
 		{
    			if (0)
@@ -198,23 +191,25 @@ int main(int argc, char* argv[])
 		}
   			
 		glEnable(GL_LIGHTING);
+	#endif
 
 		// Camera transformation.
 		
-		glTranslatef(0.0, 0.0, -2.0);
-		glTranslatef(0.0, (sin(t * 0.003) - 0.5) / 3.0, 0.0);
-		glRotatef(ry / 1.111, 0.0, 1.0, 0.0);
+		gxTranslatef(0.0, 0.0, 2.0);
+		gxTranslatef(0.0, (sin(t * 0.003) - 0.5) / 3.0, 0.0);
+		gxRotatef(ry / 1.111, 0.0, 1.0, 0.0);
 
 		// Enable sphere mapping. This will give a nice reflective effect.
 		
-		glBindTexture(GL_TEXTURE_2D, envmap);
-		glEnable(GL_TEXTURE_2D);
+		gxSetTexture(envmap);
 		
+	#if 0 // todo
 		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);		
 		glEnable(GL_TEXTURE_GEN_S);
 		glEnable(GL_TEXTURE_GEN_T);
-
+	#endif
+	
 		if (0)
 		{
 			// Cannot enable culling while mirroring too, because normals aren't mirrored as well..		
@@ -227,35 +222,38 @@ int main(int argc, char* argv[])
 		
 		// Draw floor.
 	
-		glBindTexture(GL_TEXTURE_2D, texmap);
+		gxSetTexture(texmap);
   	
 		glDepthMask(0);		// We disable writing to the depth buffer. When drawing multiple unsorted transparent surfaces, you will need to do this or suffer the consequences.
 
-		glDisable(GL_LIGHTING);
-		glDisable(GL_TEXTURE_GEN_S);
-		glDisable(GL_TEXTURE_GEN_T);
+	// todo
+		//glDisable(GL_LIGHTING);
+		//glDisable(GL_TEXTURE_GEN_S);
+		//glDisable(GL_TEXTURE_GEN_T);
 	
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_BLEND);	
-		glColor4f(1.0, 1.0, 1.0, 0.5);
+		gxColor4f(1.0, 1.0, 1.0, 0.5);
 	
-		glBegin(GL_QUADS);
+		gxBegin(GL_QUADS);
 		{
 	
-			glTexCoord2f(0.0, 0.0);
-			glVertex3f(-2.0, -1.0, -2.0);
-			glTexCoord2f(1.0, 0.0);
-			glVertex3f(+2.0, -1.0, -2.0);
-			glTexCoord2f(1.0, 1.0);
-			glVertex3f(+2.0, -1.0, +2.0);
-			glTexCoord2f(0.0, 1.0);
-			glVertex3f(-2.0, -1.0, +2.0);   
+			gxTexCoord2f(0.0, 0.0);
+			gxVertex3f(-2.0, -1.0, -2.0);
+			gxTexCoord2f(1.0, 0.0);
+			gxVertex3f(+2.0, -1.0, -2.0);
+			gxTexCoord2f(1.0, 1.0);
+			gxVertex3f(+2.0, -1.0, +2.0);
+			gxTexCoord2f(0.0, 1.0);
+			gxVertex3f(-2.0, -1.0, +2.0);
 			
-		} glEnd();
+		}
+		gxEnd();
 	
-		glEnable(GL_TEXTURE_GEN_S);
-		glEnable(GL_TEXTURE_GEN_T);		
-		glEnable(GL_LIGHTING);
+	// todo
+		//glEnable(GL_TEXTURE_GEN_S);
+		//glEnable(GL_TEXTURE_GEN_T);
+		//glEnable(GL_LIGHTING);
 	
 		glDisable(GL_BLEND);
 	
@@ -264,19 +262,18 @@ int main(int argc, char* argv[])
 //--------------------------------------------------------------------
 // Make back buffer visible.
 		
-		allegro_gl_flip();
+		framework.endDraw();
 	
 	}
 
 //--------------------------------------------------------------------
 // Shutdown system.
 
-	remove_allegro_gl();
-	allegro_exit();
+	framework.shutdown();
 	
 	return 0;
 
-} END_OF_MAIN();
+}
 
 //--------------------------------------------------------------------
 // Triangles drawer.
@@ -286,11 +283,52 @@ static void draw_triangles(int num, CIsosurfaceVertex* v)
 
 	// Use line mode?
 	
-	if (key[KEY_L])
+	if (keyboard.isDown(SDLK_l))
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	else		
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);		
-		
+	
+#if 1
+
+	// Draw vertex arrays.
+	
+	gxBegin(GL_TRIANGLES);
+	{
+		for (int i = 0; i < num * 3; ++i)
+		{
+			gxTexCoord2f(v[i].n[0], v[i].n[1]); // fixme : sphere map it
+			
+			gxNormal3fv(v[i].n);
+			gxVertex3fv(v[i].p);
+		}
+	}
+	gxEnd();
+	
+	// Mirror y coordinate.
+	
+	for (int i = 0; i < num * 3; ++i)
+	{
+	
+		v[i].p[1] = -v[i].p[1] - 2.0;
+	
+	}
+	
+	// Draw vertex arrays a second time.
+	
+	gxBegin(GL_TRIANGLES);
+	{
+		for (int i = 0; i < num * 3; ++i)
+		{
+			gxTexCoord2f(v[i].n[0], v[i].n[1]); // fixme : sphere map it
+			
+			gxNormal3fv(v[i].n);
+			gxVertex3fv(v[i].p);
+		}
+	}
+	gxEnd();
+	
+#else
+	
 	// Use vertex pointers to draw faster.
 	
 	glVertexPointer(3, GL_FLOAT, sizeof(CIsosurfaceVertex), v->p);
@@ -319,6 +357,8 @@ static void draw_triangles(int num, CIsosurfaceVertex* v)
 	{
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawArrays(GL_TRIANGLES, 0, num * 3);
-	}		
+	}
+	
+#endif
 	
 }
