5b6491418e31edd7b00af21d9e5ae27580f85ea2
Marcel Smit <marcel303@gmail.com>
2018-07-10 21:52:53 +0200

    - Ported CCsg3Ds test. - Added SPACE to toggle animation. - Added 1 and 2 to switch to alternate CSG modes. - Disabled mesh outline render (for now).

--- test/CCsg3Ds/main.cpp
+++ test/CCsg3Ds/main.cpp
@@ -1,5 +1,6 @@
-#include "../util/SOpenGL.h"
-#include "../../marstd2/marstd.h"
+#include "../Util/SOpenGL.h"
+#include "marstd.h"
+#include "framework.h"
 
 static CVector position;
 static CVector rotation;
@@ -13,39 +14,45 @@ static void renderMesh(CMesh& mesh);
 int main(int argc, char* argv[])
 {
 
-	allegro_init();
-	install_keyboard();
-	install_mouse();
-
-	SOpenGL::I().setGraphicsMode(640, 480, 32, false);
+	framework.enableDepthBuffer = true;
+	
+	if (!framework.init(0, nullptr, 640, 480))
+		exit(-1);
 
 	position[2] = 4.0;
+	
+	bool animate = true;
 
-	while (!key[KEY_ESC])
+	while (!keyboard.wentDown(SDLK_ESCAPE))
 	{
 
+		framework.process();
+		
 		static float t = 0.0;
 
-		if (key[KEY_LEFT])
-			position[0] += 0.05;
-		if (key[KEY_RIGHT])
+		if (keyboard.wentDown(SDLK_SPACE))
+			animate = !animate;
+		
+		if (keyboard.isDown(SDLK_LEFT))
 			position[0] -= 0.05;
+		if (keyboard.isDown(SDLK_RIGHT))
+			position[0] += 0.05;
 
-		if (key[KEY_A])
+		if (keyboard.isDown(SDLK_a))
 			position[1] += 0.05;
-		if (key[KEY_Z])
+		if (keyboard.isDown(SDLK_z))
 			position[1] -= 0.05;
 
-		if (key[KEY_DOWN])
+		if (keyboard.isDown(SDLK_DOWN))
 			position[2] += 0.05;
-		if (key[KEY_UP])
+		if (keyboard.isDown(SDLK_UP))
 			position[2] -= 0.05;
 
 		rotation[0] = t * 10.0;
 		rotation[1] = t * 11.0;
 		rotation[2] = t * 13.0;
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		framework.beginDraw(0, 0, 0, 0);
 
 		glDepthFunc(GL_LEQUAL);
 		glEnable(GL_DEPTH_TEST);
@@ -53,13 +60,13 @@ int main(int argc, char* argv[])
 
 		SOpenGL::I().setupStandardMatrices(0.0);
 
-		glTranslatef(-position[0], -position[1], -position[2]);
-		glRotatef(t * 10.0, 1.0, 0.0, 0.0);
-		glRotatef(t * 10.0, 0.0, 1.0, 0.0);
-		glRotatef(t * 10.0, 0.0, 0.0, 1.0);
+		gxTranslatef(-position[0], -position[1], -position[2]);
+		gxRotatef(t * 10.0, 1.0, 0.0, 0.0);
+		gxRotatef(t * 10.0, 0.0, 1.0, 0.0);
+		gxRotatef(t * 10.0, 0.0, 0.0, 1.0);
 
 		glBlendFunc(GL_ONE, GL_ONE);
-//		glEnable(GL_BLEND);
+		glDisable(GL_BLEND);
 
 		CMesh* mesh1 = new CMesh;
 		CMesh* mesh2 = new CMesh;
@@ -91,10 +98,13 @@ int main(int argc, char* argv[])
 		CCsg3D::I().subtraction(mesh1, mesh2, mesh3);
 //		CCsg3D::I().intersection(mesh1, mesh2, mesh3);
 		// Mesh 3 & 4 -> Mesh 5.
-		CCsg3D::I().addition(mesh3, mesh4, mesh5);
-//		CCsg3D::I().subtraction(mesh3, mesh4, mesh5);
-//		CCsg3D::I().intersection(mesh3, mesh4, mesh5);
-//		mesh3->move(*mesh5);
+		if (keyboard.isDown(SDLK_1))
+			CCsg3D::I().intersection(mesh3, mesh4, mesh5);
+		else if (keyboard.isDown(SDLK_2))
+			CCsg3D::I().subtraction(mesh3, mesh4, mesh5);
+		else
+			CCsg3D::I().addition(mesh3, mesh4, mesh5);
+//		mesh3->move(mesh5);
 		
 		printf("PC: %d - OP: %f %f %f\n", mesh5->polyCount, objectPosition[0], objectPosition[1], objectPosition[2]);
 		
@@ -104,7 +114,7 @@ int main(int argc, char* argv[])
 		
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		
-		renderMesh(*mesh5);
+		//renderMesh(*mesh5);
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     		
@@ -114,23 +124,25 @@ int main(int argc, char* argv[])
 		delete mesh4;
 		delete mesh5;
 
-		SOpenGL::I().flip();
+		framework.endDraw();
 
-		objectPosition[0] += 0.001;
-		objectPosition[1] += 0.001;
-		objectPosition[2] += 0.001;
-		objectRotation[0] += 0.003;
-		objectRotation[1] += 0.002;
-		objectRotation[2] += 0.001;  		
-		
-		t += 0.01;
+		if (animate)
+		{
+			objectPosition[0] += 0.001;
+			objectPosition[1] += 0.001;
+			objectPosition[2] += 0.001;
+			objectRotation[0] += 0.003;
+			objectRotation[1] += 0.002;
+			objectRotation[2] += 0.001;
+			
+			t += 0.01;
+		}
 
 	}
 
 	return 0;
 
 }
-END_OF_MAIN();
 
 static void doRenderBsp(CBsp* bsp)
 {
@@ -174,20 +186,20 @@ static void renderPoly(CPoly* poly)
 	float tmp[3];
 	for (int i = 0; i < 3; ++i)
 		tmp[i] = (poly->plane.normal[i] + 1.0) * 0.5;
-	glColor3fv(tmp);
+	setColorf(tmp[0], tmp[1], tmp[2]);
 
-	glBegin(GL_POLYGON);
+	gxBegin(GL_TRIANGLE_FAN);
 	{
 
 		for (CEdge* edge = poly->edgeHead; edge; edge = edge->next)
 		{
 
-			glVertex3f(edge->p[0], edge->p[1], edge->p[2]);
+			gxVertex3f(edge->p[0], edge->p[1], edge->p[2]);
 
 		}
 
 	}
-	glEnd();
+	gxEnd();
 
 }
 
@@ -197,35 +209,35 @@ static void renderMesh(CMesh& mesh)
     for (CPoly* poly = mesh.polyHead; poly; poly = poly->next)
     {
 
-        glColor3ub(
+        gxColor3ub(
             int((poly->plane.normal[0] + 1.0) * 0.5 * 255),
             int((poly->plane.normal[1] + 1.0) * 0.5 * 255),
             int((poly->plane.normal[2] + 1.0) * 0.5 * 255));
 
-//		glColor3ub(63, 63, 63);
+//		gxColor3ub(63, 63, 63);
 		
-        glBegin(GL_POLYGON);
+        gxBegin(GL_TRIANGLE_FAN);
         {
             for (CEdge* edge = poly->edgeHead; edge; edge = edge->next)
             {
-                glVertex3fv(edge->p);
+                gxVertex3fv(edge->p);
             }
         }
-        glEnd();
+        gxEnd();
         
-        glColor3ub(255, 255, 255);
+        gxColor3ub(255, 255, 255);
         
-        glBegin(GL_LINES);
+        gxBegin(GL_LINES);
         {
             for (CEdge* edge = poly->edgeHead; edge; edge = edge->next)
             {
             	CVector p1 = edge->p;
             	CVector p2 = edge->p + poly->plane.normal * 0.1;
-                glVertex3fv(p1);            	
-                glVertex3fv(p2);
+                gxVertex3fv(p1);
+                gxVertex3fv(p2);
             }
         }
-        glEnd();        
+        gxEnd();
         
     }
         
